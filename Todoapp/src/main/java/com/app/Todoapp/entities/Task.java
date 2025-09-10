bd65b97b7e157f5d58e70a2956320a1fb5e1993e
vineet <vsvihaan.exe@gmail.com>
2025-09-10 22:19:40 +0530

    initialization of entities and repository

---
+++ Todoapp/src/main/java/com/app/Todoapp/entities/Task.java
@@ -0,0 +1,19 @@
+package com.app.Todoapp.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+@Entity
+@Data
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private boolean completed;
+
+}
