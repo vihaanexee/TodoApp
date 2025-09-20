f3f2c9eb8f718419f3339660fad7bf0362e8ed8c
vineet <vsvihaan.exe@gmail.com>
2025-09-20 13:40:07 +0530

    starting with templates

--- Todoapp/src/main/java/com/app/Todoapp/controller/controller.java
+++ Todoapp/src/main/java/com/app/Todoapp/controller/controller.java
@@ -1,14 +1,27 @@
 package com.app.Todoapp.controller;
 
+import org.springframework.ui.Model;     // ✅ CORRECT! This is from Spring
+import com.app.Todoapp.entities.Task;
 import com.app.Todoapp.services.TaskService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 
 @Controller
+@RequestMapping("/tasks")
 public class controller     {
     private final TaskService taskService;
 
     public controller(TaskService taskService) {
         this.taskService = taskService;
+    }
+@GetMapping
+    public String getTasks(Model model){
+        List<Task> tasks = taskService.getAllTasks();
+        model.addAttribute("tasks" , tasks);
+        return "tasks";
     }
 }
--- Todoapp/src/main/java/com/app/Todoapp/services/TaskService.java
+++ Todoapp/src/main/java/com/app/Todoapp/services/TaskService.java
@@ -1,7 +1,20 @@
 package com.app.Todoapp.services;
 
+import com.app.Todoapp.entities.Task;
+import com.app.Todoapp.repository.TaskRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TaskService {
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
 }
